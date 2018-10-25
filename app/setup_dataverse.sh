eca619df7e96e2934cf5e3faac34d6a07c6f62b2
Thibault Coupin <thibault.coupin@gmail.com>
2018-10-25 14:26:59 +0200

    Force utf8 in glassfish

--- app/setup_dataverse.sh
+++ app/setup_dataverse.sh
@@ -12,5 +12,9 @@ mv /opt/dv/dvinstall/default.config.new /opt/dv/dvinstall/default.config
 cd /opt/dv/dvinstall/
 
 /opt/glassfish4/bin/asadmin start-domain
+
+## Temp workaround for https://github.com/IQSS/dataverse/issues/5234
+/opt/glassfish4/bin/asadmin create-jvm-options "-Dfile.encoding=UTF8"
+
 ./install -y -f
 /opt/glassfish4/bin/asadmin stop-domain
